79faa78f03e13d600a107e91f7c56756a2b09521
Super User <root@localhost.localdomain>
2026-05-27 06:15:26 +0300

    Fix code and CI

--- src/virtblk.c
+++ src/virtblk.c
@@ -169,9 +169,7 @@ static int __init ram_virtblk_init(void)
 	/* vzalloc - выделяет память в виртуально непрерывном адресном пространстве ядра                                             */
 	/* Используем vzalloc а не kmalloc потому что vzalloc не требует физически непрерывной памяти, что важно для больших буферов */
 	ram_data = vzalloc(DEVICE_SIZE);
-	if (!ram_data) {
-		pr_err("%s: couldn't allocate RAM (%d bytes)\n",
-		       DEVICE_NAME, DEVICE_SIZE);
+	if (!ram_data)
 		return -ENOMEM;
 	}
 
